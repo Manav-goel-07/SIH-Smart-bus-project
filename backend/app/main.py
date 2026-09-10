543c99ecf22a96edcf3ee3b79981d41058516b98
Rehan Garg <rehangrg2007@gmail.com>
2026-09-11 00:32:20 +0530

    Added origin for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5174",
         "sih-smart-bus-project.vercel.app",
+        "https://sih-smart-bus-project-8fy9.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
